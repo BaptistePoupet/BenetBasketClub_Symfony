73e0a707c1b381c2649c15f2d479a81d3b0a8f47
Baptiste POUPET <baptiste.poupet2022@campus-eni.fr>
2023-09-08 10:00:45 +0200

    cvdshvbckhdqvchkd

--- src/Controller/ActualiteController.php
+++ src/Controller/ActualiteController.php
@@ -10,39 +10,39 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Actualite;
 
-#[Route('/bien', name: 'app_bien')]
+#[Route('/actualite', name: 'app_actualite')]
 
 class ActualiteController extends AbstractController
 {
     #[Route('/liste', name: '_liste')]
-    public function index(ActualiteRepository $bienRepository): Response
+    public function index(ActualiteRepository $actualiteRepository): Response
     {
-        return $this->render('bien/index.html.twig', [
-            'biens' => $bienRepository->findAll(),
+        return $this->render('actualite/index.html.twig', [
+            'actualites' => $actualiteRepository->findAll(),
         ]);
     }
     #[Route('/{id}', name: '_voir', requirements: ['id' => '\d+'])]
-    public function voir(Actualite $bien): Response
+    public function voir(Actualite $actualite): Response
     {
-        return $this->render('bien/voir.html.twig', [
-            'bien' => $bien,
+        return $this->render('actualite/voir.html.twig', [
+            'actualite' => $actualite,
         ]);
     }
 
     #[Route('/{id}', name: '_detail', requirements: ['id' => '\d+'])]
     public function detail(int $id): Response
     {
-        return $this->render('bien/detail.html.twig', [
+        return $this->render('actualite/detail.html.twig', [
             
             'id' => $id,
         ]);
         
     }
-    #Cree une route /admin/bien/ajouter#
+    #Cree une route /admin/actualite/ajouter#
     #[Route('/ajouter', name: '_ajouter')]
     public function ajouter(): Response
     {
-        return $this->render('bien/ajouter.html.twig', [
+        return $this->render('actualite/ajouter.html.twig', [
             'controller_name' => 'ActualiteController',
         ]);
     }
