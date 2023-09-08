6bfdd08548e6cee146bbe083937cc76f9660fb15
Baptiste POUPET <baptiste.poupet2022@campus-eni.fr>
2023-09-08 12:25:17 +0200

    jdbfjcdb

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -15,4 +15,31 @@ class ContactController extends AbstractController
             'controller_name' => 'ContactController',
         ]);
     }
+    #[Route('/contacter/envoyer_formulaire', name: 'contacter_envoyer_formulaire', methods: ['POST'])]
+    public function envoyerFormulaire(Request $request, MailerInterface $mailer): Response
+    {
+        // Récupérer les données du formulaire
+        $nom = $request->request->get('nom');
+        $prenom = $request->request->get('prenom');
+        $mail = $request->request->get('mail');
+        $description = $request->request->get('description');
+
+        // Créer le contenu de l'e-mail
+        $message = "Nom: $nom\nPrénom: $prenom\nAdresse e-mail: $mail\nDescription: $description";
+
+        // Créer l'e-mail
+        $email = (new Email())
+            ->from('votre_adresse_email@example.com')
+            ->to('dirigieants.benetbasketclub@gmail.com')
+            ->subject('Nouveau formulaire soumis')
+            ->text($message);
+
+        // Envoyer l'e-mail
+        $mailer->send($email);
+
+        // Rediriger ou afficher un message de confirmation
+        // ...
+
+        return $this->redirectToRoute('page_de_confirmation');
+    }
 }
--- src/Controller/EquipeController.php
+++ src/Controller/EquipeController.php
@@ -5,14 +5,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Equipe;
+use App\Repository\EquipeRepository;   
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\EquipeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class EquipeController extends AbstractController
 {
     #[Route('/equipe', name: 'app_equipe')]
-    public function index(): Response
+    public function index(EquipeRepository $equipeRepository): Response
     {
         return $this->render('equipe/index.html.twig', [
-            'controller_name' => 'EquipeController',
+            'equipes' => $equipeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/equipe/{id}', name: 'equipe_voir', requirements: ['id' => '\d+'])]
+    public function voir(Equipe $equipe): Response
+    {
+        return $this->render('equipe/voir.html.twig', [
+            'equipe' => $equipe,
+        ]);
+    }
+
+    #[Route('/equipe/{id}', name: 'equipe_detail', requirements: ['id' => '\d+'])]
+    public function detail(int $id): Response
+    {
+        return $this->render('equipe/detail.html.twig', [
+            'id' => $id,
         ]);
     }
 }
